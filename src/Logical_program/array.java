bd0fd4f50f488dda75bbe684612b835137406f89
Dell G3 <Dell G3@DESKTOP-CV0KU3C>
2021-12-27 10:24:57 +0530

    adding

---
+++ src/Logical_program/array.java
@@ -0,0 +1,23 @@
+package Logical_program;
+import java.io.FileInputStream;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class array {
+	
+	public static void main(String[] args) {
+		
+		//Remove Duplicates from array:    
+		
+		String[] Input = {"1", "2"," 3" ,"4" ," 1" ,"9", "8", "9" , "9" , "0" , "0" , "5", "6", "7"};
+		String[] output = {"1", "2", "3" ,"4","9", "8", "0" , "5", "6", "7"};
+		
+			
+			
+			
+	}
+	
+	
+
+}
